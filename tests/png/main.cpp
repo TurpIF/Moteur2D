f160de34e9a462d7b00e35cd81a919f6bec20704
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-08-01 16:48:21 +0200

    Test if png are loaded

--- tests/png/main.cpp
+++ tests/png/main.cpp
@@ -1,9 +1,50 @@
 #include "texture.h"
 
 #include <iostream>
+#include <GL/glu.h>
+#include <GL/glut.h>
 
-int main() {
+static void draw(void) {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glRasterPos3f(0.2f, -0.8f, -1.5f);
+
     glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 1);
+    glBegin(GL_POLYGON);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(0.8f, 0.8f, -2.0f);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(0.2f, 0.8f, -100.0f);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(0.2f, 0.2f, -100.0f);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(0.8f, 0.2f, -2.0f);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char ** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutCreateWindow("Test");
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, 1.0, 0.1, 1000.0);
+    glMatrixMode(GL_MODELVIEW);
+    glDisable(GL_DITHER);
+    // glEnable(GL_TEXTURE_2D);
+    // glShadeModel(GL_SMOOTH);
+    // glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+    // glClearDepth(1.0f);
+    // glEnable(GL_DEPTH_TEST);
+    // glDepthFunc(GL_LEQUAL);
+    // glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
     for(unsigned int i = 0 ; i < 10 ; i++)
     {
         {
@@ -23,5 +64,8 @@ int main() {
                 << std::endl;
         }
     }
+
+    glutDisplayFunc(draw);
+    glutMainLoop();
     return 0;
 }
